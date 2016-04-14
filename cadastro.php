be370098c340836553d9a4bc9f3cecea4f60a3e9
João Carlos S. Heringer <jcheringer88@hotmail.com>
2016-04-14 19:39:13 -0300

    Ajustes tela de cadastro

--- cadastro.php
+++ cadastro.php
@@ -303,11 +303,10 @@
 
 			$("#novo").click(function() {
 				$("#save").removeAttr("disabled");
+				$("button[id*='btnPront']").attr("class", "btn btn-default");
 				
-				$(':text','#divQuestionario')
-				  .val('')
-			});
-			
+				LimpaCampos();
+			});			
 			
 			$("#save").click(function() {
 // 				var contVazios = 0;
@@ -373,6 +372,9 @@
 				source: "ajax/cliente.ajax.php",
 				minLength: 2,
 				select: function(event, ui) {					
+
+					LimpaProntuario();
+					LimpaCampos();
 					
 					if(ui.item) {
 						$.ajax({
@@ -494,12 +496,40 @@
 							}
 						}
 					}
-				});
-
-
-				
+				});				
 			});
 		});
+
+		function LimpaProntuario() {
+			$("#divHistoricoProntuarios").html("");
+		}
+
+		function LimpaCampos() {
+
+			$("div[data-tipo='resposta'] input[type='text']").val("");
+			
+			$("div[data-tipopergunta='Ind_Pergunta_SimNao']").each(function(){					
+
+				var optSim = $("input[type='radio'][value=1]", $(this));
+				var lblSim = optSim.parent();
+				
+				var optNao = $("input[type='radio'][value=0]", $(this));
+				var lblNao = optNao.parent();
+				
+				optSim.removeAttr("checked");
+				optSim.prop("checked", false);
+				
+				lblSim.removeClass("btn-primary");
+				lblSim.removeClass("active");
+				lblSim.addClass("btn-default");
+				
+				optNao.attr("checked", "checked");
+				optNao.prop("checked", true);
+				lblNao.removeClass("btn-default");
+				lblNao.addClass("btn-primary");					
+				lblNao.addClass("active");					
+			});
+		}		
 
 		function MontaJSON(){
 			var obj = [];
