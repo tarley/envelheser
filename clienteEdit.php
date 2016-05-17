60a4e9df192c841f33094b896f1b7f992b201fcc
Carina <Carina@Carina>
2016-05-17 13:18:13 -0300

    Correção Radio Button Prontuário #21

--- clienteEdit.php
+++ clienteEdit.php
@@ -134,7 +134,7 @@
 				for ($j = 0; $j < sizeof($listaOpcoes); $j++) {
 					echo '<div class="col-md-6 nopadding">';
 					echo '<label class="control-label col-md-2">' . $listaOpcoes[$j]['Des_Item_Radio'] . '</label>';
-					echo '<input id="Ind_Pergunta_Radio[]" name="Ind_Pergunta_Radio[]" type="radio" value="'.$listaOpcoes[$j]['Cod_Item_Radio'].'" class="form-horizontal" >';
+					echo '<input id="Ind_Pergunta_Radio[]" name="Ind_Pergunta_Radio'.$listaPerguntas[$i]['Cod_Pergunta'].'[]" type="radio" value="'.$listaOpcoes[$j]['Cod_Item_Radio'].'" class="form-horizontal" >';
 					echo '</div>';
 				}
 				
